7cdab20f0142836ba70c5519c0b8416a9dee6be4
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-06-14 14:40:08 +0200

    Inicijalizovan projekat u Spring Boot-u

---
+++ src/main/java/rs/dp/sa/centar/CentarApplication.java
@@ -0,0 +1,13 @@
+package rs.dp.sa.centar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentarApplication.class, args);
+	}
+
+}
