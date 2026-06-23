899b9349b48920258a0253b6e90256115ede232a
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-06-23 17:21:59 +0200

    Dokumentovane klase StavkaRezervacije i Racun

--- src/main/java/rs/dp/sa/centar/entity/Racun.java
+++ src/main/java/rs/dp/sa/centar/entity/Racun.java
@@ -5,6 +5,11 @@ import lombok.*;
 
 import java.util.Date;
 
+/**
+ * Predstavlja fiskalni racun koji se kreira za rezervaciju sa svim terminima, i izracunatim ukupnim iznosom
+ * Poseduje takodje datum izdavanja racuna, kao i status da li je placen racun
+ * @author Dusan Petrovic
+ */
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,20 +19,35 @@ import java.util.Date;
 @ToString
 public class Racun {
 
+    /**
+     * Jedinstveni identifikator racuna u bazi
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "racun_id")
     private Long racunId;
 
+    /**
+     * Datum kada je racun izdat
+     */
     @Column(name = "datum_izdavanja", nullable = false)
     private Date datumIzdavanja;
 
+    /**
+     * Ukupan iznos rezervacije izrazen u dinarima
+     */
     @Column(name = "ukupan_iznos", nullable = false)
     private Double ukupanIznos;
 
+    /**
+     * Status da li je racun placen ili nije
+     */
     @Column(name = "status_placanja", nullable = false)
     private String statusPlacanja;
 
+    /**
+     * Rezervacija na koju se racun odnosi
+     */
     @OneToOne
     @JoinColumn(name = "rezervacija_id", nullable = false, unique = true)
     private RezervacijaTermina rezervacijaTermina;
--- src/main/java/rs/dp/sa/centar/entity/StavkaRezervacije.java
+++ src/main/java/rs/dp/sa/centar/entity/StavkaRezervacije.java
@@ -3,6 +3,12 @@ package rs.dp.sa.centar.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+/**
+ * Predstavlja stavku rezervacije koja sadrzi informacije o ceni, opcionoj napomeni korisnika,
+ * broju sati koliko ce biti zakupljena sala i rezervaciju termina
+ * na koji se odnosi stavka, kao i sali na koju se odnosi
+ * @author Dusan Petrovic
+ */
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,24 +18,42 @@ import lombok.*;
 @ToString
 public class StavkaRezervacije {
 
+    /**
+     * Jedinstveni identifikator stavke rezervacije u bazi
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "stavka_id")
     private Long stavkaId;
 
+    /**
+     * Cena iznajmljivanja termina izrazena u dinarima
+     */
     @Column(name = "cena", nullable = false)
     private Double cena;
 
+    /**
+     * Opciona napomena korisnika prilikom rezervisanja termina u nekoj sali
+     */
     @Column(name = "napomena")
     private String napomena;
 
+    /**
+     * Broj sati na koliko ce odredjena sala biti zakupljena
+     */
     @Column(name = "broj_sati", nullable = false)
     private Integer brojSati;
 
+    /**
+     * Rezervacija kojoj stavka pripada
+     */
     @ManyToOne
     @JoinColumn(name = "rezervacija_id", nullable = false)
     private RezervacijaTermina rezervacijaTermina;
 
+    /**
+     * Sala koja ce biti zakupljena
+     */
     @ManyToOne
     @JoinColumn(name = "sala_id", nullable = false)
     private Sala sala;
