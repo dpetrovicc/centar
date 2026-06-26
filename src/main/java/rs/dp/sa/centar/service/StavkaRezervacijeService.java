4435ca9c768a932b0d3b01044f43093b4a88abf6
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-06-26 19:01:00 +0200

    Dokumentovan StavkaRezervacijeService servis

--- src/main/java/rs/dp/sa/centar/service/StavkaRezervacijeService.java
+++ src/main/java/rs/dp/sa/centar/service/StavkaRezervacijeService.java
@@ -13,16 +13,41 @@ import rs.dp.sa.centar.repository.RezervacijaTerminaRepository;
 import rs.dp.sa.centar.repository.SalaRepository;
 import rs.dp.sa.centar.repository.StavkaRezervacijeRepository;
 
+/**
+ * Servis klasa za upravljanje stavkama rezervacije
+ * Sadrzi logiku za kreiranje, tj dodavanje novih stavki u okviru postojece rezervacije,
+ * pri cemu automatski azurira ukupnu vrednost rezervacije
+ *
+ * @author Dusan Petrovic
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 
 public class StavkaRezervacijeService {
 
+    /**
+     * Repozitorijum za rad sa podacima o stavkama rezervacije
+     */
     private final StavkaRezervacijeRepository srRepository;
+    /**
+     * Repozitorijum za rad sa podacima o rezervacijama
+     */
     private final RezervacijaTerminaRepository rtRepository;
+    /**
+     * Repozitorijum za rad sa podacima o salama
+     */
     private final SalaRepository sRepository;
 
+    /**
+     * Kreiranje nove stavke rezervacije u okviru postojece rezervacije
+     * Proverava da li prosledjena rezervacija postoji u bazi, kao i sala koja treba da se doda,
+     * racuna se cena nove stavke, i azurira ukupna cena rezervacije
+     *
+     * @param request DTO objekat koji sadrzi ID rezervacije, ID sale, broj sati i napomenu
+     * @return StavkaRezervacijeResponse DTO objekat sa kreiranom stavkom i generisanim ID-em
+     * @throws java.lang.RuntimeException ukoliko ne postoji prosledjena rezervacija, ili prosledjena sala u bazi
+     */
     @Transactional
     public StavkaRezervacijeResponse create(StavkaRezervacijeRequest request){
         RezervacijaTermina rez = rtRepository.findById(request.rezervacijaId()).
