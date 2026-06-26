6f9c297651bbcf8e4260bdc18181475e0a0500ed
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-06-26 19:07:38 +0200

    Dokumentovan RacunService servis

--- src/main/java/rs/dp/sa/centar/service/RacunService.java
+++ src/main/java/rs/dp/sa/centar/service/RacunService.java
@@ -13,14 +13,37 @@ import rs.dp.sa.centar.repository.RezervacijaTerminaRepository;
 
 import java.util.Date;
 
+/**
+ * Servis klasa za upravljanje racunima u sportskom centru
+ * Sadrzi logiku za kreiranje novog racuna na osnovu rezervacije i svih njenih stavki
+ *
+ * @author Dusan Petrovic
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RacunService {
 
+    /**
+     * Repozitorijum za rad sa podacima o racunima
+     */
     private final RacunRepository rRepository;
+    /**
+     * Repozitorijum za rad sa podacima o rezervacijama
+     */
     private final RezervacijaTerminaRepository rtRepository;
 
+    /**
+     * Kreiranje novog racuna na osnovu prosledjene rezervacije
+     * Proverava da li prosledjena rezervacija postoji u bazi na osnovu ID-a
+     * Ukupnu cenu racuna uzima i postavlja na ukupan iznos racuna
+     * Postavlja datum izdavanja na trenutni datum i inijcalizuje status placanja na "Nije placeno"
+     *
+     * @param request DTO objekat koji sadrzi jedinstveni identifikator rezervacije
+     * da bi znao na koju rezeraciju se odnosi racun
+     * @return RacunResponse DTO objekat sa podacima o kreiranom racunu i generisanim ID-em
+     * @throws java.lang.RuntimeException ukoliko prosledjena rezervacija ne postoji u bazi na osnovu ID-a
+     */
     @Transactional
     public RacunResponse create(RacunRequest request){
         RezervacijaTermina rt = rtRepository.findById(request.rezervacijaId())
