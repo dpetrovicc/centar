3fd786432beae496168d58a35f2eac27254a29e9
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-07-01 17:03:19 +0200

    Modifikovana klasa Racun

--- src/main/java/rs/dp/sa/centar/entity/Racun.java
+++ src/main/java/rs/dp/sa/centar/entity/Racun.java
@@ -1,6 +1,9 @@
 package rs.dp.sa.centar.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 import java.util.Date;
@@ -30,24 +33,29 @@ public class Racun {
     /**
      * Datum kada je racun izdat
      */
+    @NotNull(message = "Datum izdavanja racuna ne sme biti null")
     @Column(name = "datum_izdavanja", nullable = false)
     private Date datumIzdavanja;
 
     /**
      * Ukupan iznos rezervacije izrazen u dinarima
      */
+    @NotNull(message = "Ukupan iznos racuna ne sme biti null")
+    @PositiveOrZero(message = "Ukupan iznos mora biti veci ili jednak nuli")
     @Column(name = "ukupan_iznos", nullable = false)
     private Double ukupanIznos;
 
     /**
      * Status da li je racun placen ili nije
      */
+    @NotBlank(message = "Status placanja ne sme biti prazan")
     @Column(name = "status_placanja", nullable = false)
     private String statusPlacanja;
 
     /**
      * Rezervacija na koju se racun odnosi
      */
+    @NotNull(message = "Rezervacija termina mora biti dodeljena racunu")
     @OneToOne
     @JoinColumn(name = "rezervacija_id", nullable = false, unique = true)
     private RezervacijaTermina rezervacijaTermina;
