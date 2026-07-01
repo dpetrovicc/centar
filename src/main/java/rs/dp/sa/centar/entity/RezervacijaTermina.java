59526440cdcaf7ea650d9c47ff5d12bf65d24948
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-07-01 16:12:56 +0200

    Modifikovana klasa RezervacijaTermina

--- src/main/java/rs/dp/sa/centar/entity/RezervacijaTermina.java
+++ src/main/java/rs/dp/sa/centar/entity/RezervacijaTermina.java
@@ -1,6 +1,9 @@
 package rs.dp.sa.centar.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 import java.util.Date;
@@ -29,24 +32,29 @@ public class RezervacijaTermina {
     /**
      * Datum odrzavanja rezervisanog termina
      */
+    @NotNull(message = "Datum rezervacije ne sme biti null")
     @Column(name = "datum", nullable = false)
     private Date datum;
 
     /**
      * Suma svih stavki rezervacija izrazena u dinarima
      */
+    @NotNull(message = "Ukupna cena ne sme biti null")
+    @PositiveOrZero(message = "Ukupna cena mora biti veca ili jednaka nuli")
     @Column(name = "ukupna_cena")
     private Double ukupnaCena = 0.0;
 
     /**
      * Status rezervacije DA/NE
      */
+    @NotBlank(message = "Status odobrenja ne sme biti prazan")
     @Column(name = "odobreno", nullable = false)
     private String odobreno = "NE";
 
     /**
      * Korisnik na kog glasi rezervacija
      */
+    @NotNull(message = "Korisnik mora biti dodeljen rezervaciji")
     @ManyToOne
     @JoinColumn(name = "korisnik_id", nullable = false)
     private Korisnik korisnik;
@@ -54,6 +62,7 @@ public class RezervacijaTermina {
     /**
      * Sportski centar na koji se odnosi rezervacija
      */
+    @NotNull(message = "Sportski centar mora biti dodeljen rezervaciji")
     @ManyToOne
     @JoinColumn(name = "sportski_centar_id", nullable = false)
     private SportskiCentar sportskiCentar;
