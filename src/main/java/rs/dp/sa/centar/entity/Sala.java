5d76661f1bec75389cb845f9b5006432193c2c67
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-07-01 12:57:57 +0200

    Modifikovana klasa Sala

--- src/main/java/rs/dp/sa/centar/entity/Sala.java
+++ src/main/java/rs/dp/sa/centar/entity/Sala.java
@@ -1,6 +1,9 @@
 package rs.dp.sa.centar.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -27,24 +30,29 @@ public class Sala {
     /**
      * Naziv sale u sportskom centru
      */
+    @NotBlank(message = "Naziv sale ne sme biti prazan")
     @Column(name = "naziv")
     private String naziv;
 
     /**
      * Da li je sala na otvorenom ili ne
      */
+    @NotBlank(message = "Polje na otvorenom ne sme biti prazno")
     @Column(name = "na_otvorenom")
     private String naOtvorenom;
 
     /**
      * Cena koriscenja sale po satu izrazena u dinarima
      */
+    @NotNull(message = "Cena po satu mora biti uneta")
+    @Min(value = 1, message = "Cena po satu mora biti najmanje 1")
     @Column(name = "cena_po_satu")
     private Double cenaPoSatu;
 
     /**
      * Sportski centar u kom se sala nalazi
      */
+    @NotNull(message = "Sportski centar mora biti dodeljen sali")
     @ManyToOne
     @JoinColumn(name = "sportski_centar_id", nullable = false)
     private SportskiCentar sportskiCentar;
